302cad6bbd8a93117bd67db10897df9ee70d348a
Aoife Woodrow <woodrow-a@email.ulster.ac.uk>
2020-11-28 20:58:17 +0000

    Added GIT Hub URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,10 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return  <div className="App">Weather app</div>;
+  return  <div className="App">
+    <h1>Weather App</h1>
+    <footer>
+      <a href="https://github.com/aoife-woodrow/React-Weather-App" target="_blank">Open source code</a> by Aoife Woodrow
+    </footer>
+  </div>;
 }
