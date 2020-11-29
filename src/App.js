4a44df346dfd528cdc978a1bdff79a5e960bc371
Aoife Woodrow <woodrow-a@email.ulster.ac.uk>
2020-11-29 09:58:39 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return  <div className="App">
-    <h1>Weather App</h1>
-    <footer>
-      <a href="https://github.com/aoife-woodrow/React-Weather-App" target="_blank">Open source code</a> by Aoife Woodrow
-    </footer>
-  </div>;
+  return (
+    <div className="container">
+      <div className="App">
+        <h1>Weather App</h1>
+          <footer>
+            <a href="https://github.com/aoife-woodrow/React-Weather-App" target="_blank" rel="noreferrer">Open source code</a> by Aoife Woodrow
+          </footer>
+      </div>
+    </div>
+  )
 }
