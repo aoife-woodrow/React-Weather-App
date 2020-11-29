7fe0e6038c0ed10cd2fad818c63308f85a50ab3f
Aoife Woodrow <woodrow-a@email.ulster.ac.uk>
2020-11-29 10:39:42 +0000

    Updated CSS

--- src/Weather.js
+++ src/Weather.js
@@ -7,10 +7,10 @@ export default function Weather() {
             <form>
                  <div class="row">
                         <div class="col-9">
-                            <input type="search" placeholder="Enter a city" className="form-control" autocomplete="off"/>
+                            <input type="search" placeholder="Enter a city" className="form-control" autoFocus="on"/>
                         </div>
                         <div class="col-3">
-                            <input type="submit" value="Search" className="btn btn-primary"/>
+                            <input type="submit" value="Search" className="btn btn-primary w-100"/>
                         </div>
                 </div>
             </form>
@@ -19,12 +19,13 @@ export default function Weather() {
                 <li>Sunday 10:00</li>
                 <li>Mostly Cloudy</li>
             </ul>
-        <div className="row">
+        <div className="row mt-3">
             <div className="col-6">
                 <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" 
                 alt="Mostly Cloudy" 
                 class="float-left" />
-                6°C
+                <span className="currentTemp">6</span>
+                <span className="unit">°C</span>
             </div>
              <div className="col-6">
                 <ul>
@@ -34,7 +35,6 @@ export default function Weather() {
                 </ul>
             </div>
         </div>
-
         </div>
     )
 }
